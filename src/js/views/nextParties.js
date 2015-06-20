a25ee86c8e5eda954f56bf9c3ca4b58e6538143f
Jerek0 <jeremy.minie@gmail.com>
2015-06-20 15:50:33 +0200

    FO Next parties

--- src/js/views/nextParties.js
+++ src/js/views/nextParties.js
@@ -4,6 +4,7 @@
 
 let Backbone = require('backbone');
 let template = require('../../htdocs/templates/nextParties.hbs');
+let $ = require('jquery');
 
 module.exports = Backbone.View.extend({
 
@@ -14,9 +15,29 @@ module.exports = Backbone.View.extend({
     initialize: function() {
         //this.render();
     },
+    
+    bindUIActions: function() {
+        this.registerPartyFilters();
+    },
+    
+    registerPartyFilters: function() {
+        let scope = this;
+        let $filters = $('.filter', this.$el);
+        $filters.on('click', function(e) {
+            e.preventDefault();
+            
+            let type = $(this).attr('data-type');
+            $(this).toggleClass('btn-secondary');
+            $(this).toggleClass('btn-primary');
+            $('.party-type-'+type, scope.$el).toggleClass('hidden');
+        });
+    },
 
     render: function() {
         this.$el.html(this.template(window.dataManager.getData()));
+        
+        this.bindUIActions();
+        
         return this;
     }
 });
